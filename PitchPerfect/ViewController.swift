5f04db1cdc3e773bf5441e97ee40384a09402a73
zetas <ddv@qubitlogic.net>
2017-06-08 19:16:33 -0400

    Add graphic assets for project. Disable and re-enable buttons as necessary when the user interacts with them. Change text when user interacts with buttons.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,25 +11,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func recordAudio(_ sender: Any) {
-        print("Record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
 
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop recording button was pressed")
-        
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
 }
 
